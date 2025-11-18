75bc7020c4efeaca4e5f12a490cf5a1e27eb21ff
Joel Cohen <joelbcohen@gmail.com>
2025-11-17 19:20:06 -0500

    latest and greatest

--- wear/src/main/java/io/callista/cloudcrypto/data/RegistrationApi.kt
+++ wear/src/main/java/io/callista/cloudcrypto/data/RegistrationApi.kt
@@ -6,9 +6,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
-import retrofit2.http.GET
 import retrofit2.http.POST
-import retrofit2.http.Query
 import java.util.concurrent.TimeUnit
 
 /**
@@ -21,11 +19,20 @@ interface RegistrationApi {
         @Body request: RegistrationRequest
     ): RegistrationResponse
 
-    @GET("public/bgc/static-response")
+    @POST("public/crypto/deregister")
     suspend fun deregisterDevice(
-        @Query("action") action: String = "deregister"
+        @Body request: DeregistrationRequest
     ): RegistrationResponse
 }
+
+/**
+ * Request body for device deregistration.
+ */
+data class DeregistrationRequest(
+    val publicKey: String,
+    val attestationBlob: String,
+    val serialNumber: String
+)
 
 /**
  * Request body for device registration.
